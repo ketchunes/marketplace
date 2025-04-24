1e5cae43d7ad5020b4b045d5e4afee9c77cb8eaa
catGirl_seeker <danildenisov572@gmail.com>
2025-04-24 21:42:16 +0500

    buttons

--- components/Clothes.vue
+++ components/Clothes.vue
@@ -8,11 +8,12 @@
           <div class="text-lg">{{ elem.title }}</div>
           <div>Коллекция 2023-2024</div>
           <div class="text-secondary-content mb-auto">{{ elem.price }}</div>
-          <button class="active:scale-95 rounded-md hover:cursor-pointer w-fit">
+          <button
+            class="active:scale-95 rounded-md hover:cursor-pointer w-fit transition">
             <CardHover
               border-radius="var(--radius-box)"
-              container-color="var(--color-base-200)" 
-              light-color="var(--color-secondary-content)" 
+              container-color="var(--color-base-200)"
+              light-color="var(--color-secondary-content)"
               base-color="var(--color-base-200)">
               <div class="py-2 px-4 font-semibold text-sm">
                 ДОБАВИТЬ В КОРЗИНУ
@@ -28,11 +29,12 @@
           <div class="text-lg">{{ elem.title }}</div>
           <div>Коллекция 2023-2024</div>
           <div class="text-secondary-content mb-auto">{{ elem.price }}</div>
-          <button class="active:scale-95 rounded-md hover:cursor-pointer w-fit">
+          <button
+            class="active:scale-95 rounded-md hover:cursor-pointer w-fit transition">
             <CardHover
               border-radius="var(--radius-box)"
-              container-color="var(--color-base-200)" 
-              light-color="var(--color-secondary-content)" 
+              container-color="var(--color-base-200)"
+              light-color="var(--color-secondary-content)"
               base-color="var(--color-base-200)">
               <div class="py-2 px-4 font-semibold text-sm">
                 ДОБАВИТЬ В КОРЗИНУ
@@ -48,11 +50,12 @@
           <div class="text-lg">{{ elem.title }}</div>
           <div>Коллекция 2023-2024</div>
           <div class="text-secondary-content mb-auto">{{ elem.price }}</div>
-          <button class="active:scale-95 rounded-md hover:cursor-pointer w-fit">
+          <button
+            class="active:scale-95 rounded-md hover:cursor-pointer w-fit transition">
             <CardHover
               border-radius="var(--radius-box)"
-              container-color="var(--color-base-200)" 
-              light-color="var(--color-secondary-content)" 
+              container-color="var(--color-base-200)"
+              light-color="var(--color-secondary-content)"
               base-color="var(--color-base-200)">
               <div class="py-2 px-4 font-semibold text-sm">
                 ДОБАВИТЬ В КОРЗИНУ
@@ -65,84 +68,84 @@
   </div>
 </template>
 <script setup>
-const lol = [
-  {
-    img: "img/cl-2.png",
-    title: "ПАРКА ЗИМНЯЯ ЖЕНСКАЯ «МАРИИНКА»",
-    subTitle: "Коллекция 2023-2024",
-    price: "35.000 Тг.",
-  },
-  {
-    img: "img/cl-1.png",
-    title: "ПАРКА ЗИМНЯЯ ЖЕНСКАЯ «МАРИИНКА» ФУКСИЯ",
-    subTitle: "Коллекция 2023-2024",
-    price: "30.000 Тг.",
-  },
-  {
-    img: "img/cl-3.png",
-    title: "ПАРКА ЗИМНЯЯ ЖЕНСКАЯ «МАРИИНКА» ПЕРЛАМУТР",
-    subTitle: "Коллекция 2023-2024",
-    price: "38.000 Тг.",
-  },
-  {
-    img: "img/cl-4.png",
-    title: "КУРТКА КОРОТКАЯ ЗИМНЯЯ ЖЕНСКАЯ «ФОНТАНКА» БЕЖЕВАЯ",
-    subTitle: "Коллекция 2023-2024",
-    price: "35.000 Тг.",
-  },
-];
+  const lol = [
+    {
+      img: "img/cl-2.png",
+      title: "ПАРКА ЗИМНЯЯ ЖЕНСКАЯ «МАРИИНКА»",
+      subTitle: "Коллекция 2023-2024",
+      price: "35.000 Тг.",
+    },
+    {
+      img: "img/cl-1.png",
+      title: "ПАРКА ЗИМНЯЯ ЖЕНСКАЯ «МАРИИНКА» ФУКСИЯ",
+      subTitle: "Коллекция 2023-2024",
+      price: "30.000 Тг.",
+    },
+    {
+      img: "img/cl-3.png",
+      title: "ПАРКА ЗИМНЯЯ ЖЕНСКАЯ «МАРИИНКА» ПЕРЛАМУТР",
+      subTitle: "Коллекция 2023-2024",
+      price: "38.000 Тг.",
+    },
+    {
+      img: "img/cl-4.png",
+      title: "КУРТКА КОРОТКАЯ ЗИМНЯЯ ЖЕНСКАЯ «ФОНТАНКА» БЕЖЕВАЯ",
+      subTitle: "Коллекция 2023-2024",
+      price: "35.000 Тг.",
+    },
+  ];
 
-const kek = [
-  {
-    img: "img/man-1.png",
-    title: "ПАРКА ЗИМНЯЯ МУЖСКАЯ «АДМИРАЛТЕЙСТВО» ЧЁРНАЯ",
-    subTitle: "Коллекция 2023-2024",
-    price: "31.000 Тг.",
-  },
-  {
-    img: "img/man-2.png",
-    title: "КУРТКА КОРОТКАЯ МУЖСКАЯ «ВЕРФЬ» БЕЖЕВАЯ",
-    subTitle: "Коллекция 2023-2024",
-    price: "26.400 Тг.",
-  },
-  {
-    img: "img/man-3.png",
-    title: "ВЕТРОВКА МУЖСКАЯ ЧЁРНАЯ",
-    subTitle: "Коллекция 2023-2024",
-    price: "12.000 Тг.",
-  },
-  {
-    img: "img/man-4.png",
-    title: "ПЛАЩ МУЖСКОЙ DEEP BLUE",
-    subTitle: "Коллекция 2023-2024",
-    price: "17.500 Тг.",
-  },
-];
+  const kek = [
+    {
+      img: "img/man-1.png",
+      title: "ПАРКА ЗИМНЯЯ МУЖСКАЯ «АДМИРАЛТЕЙСТВО» ЧЁРНАЯ",
+      subTitle: "Коллекция 2023-2024",
+      price: "31.000 Тг.",
+    },
+    {
+      img: "img/man-2.png",
+      title: "КУРТКА КОРОТКАЯ МУЖСКАЯ «ВЕРФЬ» БЕЖЕВАЯ",
+      subTitle: "Коллекция 2023-2024",
+      price: "26.400 Тг.",
+    },
+    {
+      img: "img/man-3.png",
+      title: "ВЕТРОВКА МУЖСКАЯ ЧЁРНАЯ",
+      subTitle: "Коллекция 2023-2024",
+      price: "12.000 Тг.",
+    },
+    {
+      img: "img/man-4.png",
+      title: "ПЛАЩ МУЖСКОЙ DEEP BLUE",
+      subTitle: "Коллекция 2023-2024",
+      price: "17.500 Тг.",
+    },
+  ];
 
-const pelmeni = [
-  {
-    img: "img/woman-1.png",
-    title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ «ЭРМИТАЖ» МЯТНОЕ",
-    subTitle: "Коллекция 2023-2024",
-    price: "17.500 Тг.",
-  },
-  {
-    img: "img/woman-2.png",
-    title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ «ЭРМИТАЖ» ОРХИДЕЯ",
-    subTitle: "Коллекция 2023-2024",
-    price: "14.000 Тг.",
-  },
-  {
-    img: "img/woman-3.png",
-    title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ «ЭРМИТАЖ» ЧЁРНОЕ",
-    subTitle: "Коллекция 2023-2024",
-    price: "19.600 Тг.",
-  },
-  {
-    img: "img/woman-4.png",
-    title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ ПЫЛЬНО-ГОЛУБОЕ",
-    subTitle: "Коллекция 2023-2024",
-    price: "15.400 Тг.",
-  },
-];
+  const pelmeni = [
+    {
+      img: "img/woman-1.png",
+      title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ «ЭРМИТАЖ» МЯТНОЕ",
+      subTitle: "Коллекция 2023-2024",
+      price: "17.500 Тг.",
+    },
+    {
+      img: "img/woman-2.png",
+      title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ «ЭРМИТАЖ» ОРХИДЕЯ",
+      subTitle: "Коллекция 2023-2024",
+      price: "14.000 Тг.",
+    },
+    {
+      img: "img/woman-3.png",
+      title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ «ЭРМИТАЖ» ЧЁРНОЕ",
+      subTitle: "Коллекция 2023-2024",
+      price: "19.600 Тг.",
+    },
+    {
+      img: "img/woman-4.png",
+      title: "ПАЛЬТО ТЁПЛОЕ ЖЕНСКОЕ ПЫЛЬНО-ГОЛУБОЕ",
+      subTitle: "Коллекция 2023-2024",
+      price: "15.400 Тг.",
+    },
+  ];
 </script>
