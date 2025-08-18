4d2932f8e58519eb1921ac21b96416035b85564f
catGirl_seeker <danildenisov572@gmail.com>
2025-08-18 14:25:20 +0500

    all

--- components/Basket/Item.vue
+++ components/Basket/Item.vue
@@ -7,7 +7,7 @@
           <div class="text-sm max-h-14 text-ellipsis overflow-hidden">{{ item.title }}</div>
           <div class="icontent">
             <div class="text-xs mb-2">Размер:</div>
-            <select name="size[<?= $items[7] ?>]" class="select select-bordered w-full max-w-xs">
+            <select name="size" class="select select-bordered w-full max-w-xs">
               <option value="m">M</option>
               <option value="l">L</option>
               <option value="xl">XL</option>
@@ -17,34 +17,15 @@
         </div>
       </div>
       <div class="xs:w-1/3 w-full flex xs:flex-col flex-row justify-around">
-        <div class="w-full flex xs:justify-end justify-center items-center">
-          <div
-            class="w-5 h-5 min-h-5 btn btn-circle btn-outline group"
-            onclick="SubstarctValue('itemQuantity<?= $items[0] ?>', 1, 'price<?= $items[0] ?>', 'prices<?= $items[0] ?>');">
-            <svg
-              width="8"
-              height="2"
-              viewBox="0 0 8 2"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
+        <div class="w-full flex gap-2 xs:justify-end justify-center items-center">
+          <div @click="quantity > 0 && quantity--" class="w-5 h-5 min-h-5 btn btn-circle btn-outline group">
+            <svg width="8" height="2" viewBox="0 0 8 2" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path class="group-hover:invert stroke-base-content" d="M0 1H7.5" />
             </svg>
           </div>
-          <input
-            id="itemQuantity<?= $items[0] ?>"
-            class="input w-8 h-fit m-0 p-0 bg-none border-none pointer-events-none text-center"
-            type="number"
-            value="1"
-            name="quantity[<?= $items[7] ?>]" />
-          <div
-            class="w-5 h-5 min-h-5 btn btn-circle btn-outline group"
-            onclick="AddValue('itemQuantity<?= $items[0] ?>', 15, 'price<?= $items[0] ?>', 'prices<?= $items[0] ?>');">
-            <svg
-              width="8"
-              height="8"
-              viewBox="0 0 8 8"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
+          <div>{{ quantity }}</div>
+          <div @click="quantity++" class="w-5 h-5 min-h-5 btn btn-circle btn-outline group">
+            <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path class="group-hover:invert stroke-base-content" d="M0 4H8" />
               <path class="group-hover:invert stroke-base-content" d="M4 0V8" />
             </svg>
@@ -52,36 +33,12 @@
         </div>
         <div class="w-full flex xs:justify-end justify-center items-center gap-2">
           <div class="grid gap-1">
-            <div class="flex flex-col gap-x-5">
-              <!-- <? if ($items[5] > 0 && $items[5] != null) { ?>
-								<div class="text-secondary">
-									<span id="price<?= $items[0] ?>" class="prices<?= $items[0] ?>"><? echo $items[4] - $items[4] / 100 * $items[5] ?> </span>
-									<span>Тг.</span>
-								</div>
-								<div class="line-through">
-									<span class="prices<?= $items[0] ?>"><? echo $items[4] ?></span> 
-									<span>Тг.</span>
-								</div>
-							<? } else { ?>
-								<span id="price<?= $items[0] ?>"><? echo $items[4] ?> <span>Тг.</span></span>
-							<? } ?> -->
-            </div>
+            <div class="flex flex-col gap-x-5"></div>
           </div>
-          <button
-            @click.prevent="deleteItemById(item.id, localItems)"
-            class="w-5 h-5 min-h-5 btn btn-circle btn-outline group">
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                class="group-hover:invert stroke-base-content"
-                d="M3.23389 8.88959L8.76555 3.11026" />
-              <path
-                class="group-hover:invert stroke-base-content"
-                d="M3.11005 3.23413L8.88938 8.7658" />
+          <button @click.prevent="deleteItemById(item.id, localItems)" class="w-5 h-5 min-h-5 btn btn-circle btn-outline group">
+            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path class="group-hover:invert stroke-base-content" d="M3.23389 8.88959L8.76555 3.11026" />
+              <path class="group-hover:invert stroke-base-content" d="M3.11005 3.23413L8.88938 8.7658" />
             </svg>
           </button>
         </div>
@@ -91,6 +48,7 @@
 </template>
 
 <script setup>
+  const quantity = ref(0);
   const localItems = useLocalStorage("items", []);
 
   defineProps({
